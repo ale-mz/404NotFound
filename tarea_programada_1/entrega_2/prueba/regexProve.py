bc50d9d934c4a59d90283db07d421c405e6103dc
Axel.Padila <Axel.Padilla@ucr.ac.cr>
2023-08-30 14:19:31 -0600

    Dates and Cipher Regex added > > Co-authored-by: Archibald Carrion  <archibald.carrion@ucr.ac.cr> Co-authored-by: Alejandro Madriz <alemadriz1@gmail.com>

--- tarea_programada_1/entrega_2/prueba/regexProve.py
+++ tarea_programada_1/entrega_2/prueba/regexProve.py
@@ -6,11 +6,19 @@ tokens = (
   'CARNET',
   'PROF',
   'IP',
+  'CLOCK',
   'PLUGIN_MSG',
   'AF_FLAG',
   'ACT_FLAG',
   'CH_MSG',
-  'FOLDER_DIR'
+  'CIPH_MSG',
+  'BIT_MSG',
+  'FOLDER_DIR',
+  'MONTH',
+  'WEEK_DAY',
+  'PORT',
+  'REG_NUM',
+  'BASIC_DIV'
 )
 
 def t_FOLDER_DIR(t):
@@ -28,6 +36,10 @@ def t_PROF(t):
 def t_IP(t):
   r"""(([0-2]([5][0-5]|[0-4][0-9])|[01]?[0-9]?[0-9])
       [\.]){3}([0-2]([5][0-5]|[0-4][0-9])|([01]?[0-9]?[0-9]))"""
+  return t
+
+def t_CLOCK(t):
+  r'(([0-5]\d|60):){2}([0-5]\d)'
   return t
 
 # Define regex for plugins messages
@@ -49,6 +61,39 @@ def t_ACT_FLAG(t):
 def t_CH_MSG(t):
   r'([A-z]([a-z])+(\s))+(Channel:)'
   return t
+# Define cipher messages
+def t_CIPH_MSG(t):
+  r'(C|c)ipher[\w\s\'-]*with'
+  return t
+# Define bit messages
+def t_BIT_MSG(t):
+  r'(B|b)it(\sRSA|\skey)'
+  return t
+
+# Define regex for log Months
+def t_MONTH(t):
+  r'Jan|Feb|Ma(r|y)|A(pr|ug)|Ju(n|l)|Sep|Oct|Nov|Dec'
+  return t
+
+# Define regex for log week day
+def t_WEEK_DAY(t):
+  r'Mon|Tue|Wed|Thu|Fri|S(at|un)'
+  return t
+
+# Define regex for Port
+def t_PORT(t):
+  r':(\d){5}'
+  return t
+
+
+# Define regex for Day dates (2 digit number)
+def t_REG_NUM(t):
+  r'(\d)+'
+  return t
+
+def t_BASIC_DIV(t):
+  r'\/'
+  return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -60,6 +105,7 @@ t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
+  # print("Illegal character '%s'" % t.value[0])
   t.lexer.skip(1)
 
 # Build the lexer
